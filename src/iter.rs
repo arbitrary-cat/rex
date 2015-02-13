e48e734b9caf515aca05dea69a7f4b337869b058
Arbitrary Cat <scpayson+devel@gmail.com>
2015-02-12 22:11:45 -0800

    Added ResultIterExt and the TakeOrErr iterator.
    These provide helpers for working with iterators of Results.

--- src/iter.rs
+++ src/iter.rs
@@ -15,6 +15,9 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+use std::error::FromError;
+use std::mem;
+
 pub struct TakeWhileInclusive<I, P>
     where I: Iterator,
           P: Fn(&<I as Iterator>::Item) -> bool {
@@ -45,6 +48,7 @@ impl<I, P> Iterator for TakeWhileInclusive<I, P>
     }
 }
 
+
 /// `RexIterExt` provides methods on top of iterators (which feel generally useful).
 pub trait RexIterExt: Iterator + Sized {
 
@@ -63,3 +67,56 @@ pub trait RexIterExt: Iterator + Sized {
 }
 
 impl<I> RexIterExt for I where I: Iterator {}
+
+pub struct TakeOrErr<I, V, E1, E2>
+    where I: Iterator<Item = Result<V, E1>>, E2: FromError<E1> {
+
+    inner:     I,
+    remaining: usize,
+    err:       Option<E2>,
+}
+
+impl<I, V, E1, E2> Iterator for TakeOrErr<I, V, E1, E2>
+    where I: Iterator<Item = Result<V, E1>>, E2: FromError<E1> {
+
+    type Item = Result<V, E2>;
+
+    fn next(&mut self) -> Option<Result<V, E2>> {
+        if self.remaining == 0 {
+            None
+        } else {
+            match self.inner.next() {
+                Some(Ok(v)) => {
+                    self.remaining -= 1;
+                    Some(Ok(v))
+                }
+
+                Some(Err(e)) => Some(Err(FromError::from_error(e))),
+
+                None => match mem::replace(&mut self.err, None) {
+                    Some(err) => Some(Err(err)),
+                    None      => None,
+                }
+            }
+        }
+    }
+}
+
+/// `ResultIterExt` provides some convenient methods for iterators which yield `Result`s.
+pub trait ResultIterExt<V, E>: Iterator<Item = Result<V, E>> + Sized {
+
+    /// `take_or_err` returns an iterator which, if the stream ends prematurely, will place an
+    /// error at the end of it to indicate this. This is intended to be used with `result::fold` to
+    /// detect EOF in iterator streams (but might have other uses).
+    fn take_or_err<E2>(self, num: usize, err: E2) -> TakeOrErr<Self, V, E, E2>
+        where E2: FromError<E> {
+
+        TakeOrErr {
+            inner:     self,
+            remaining: num,
+            err:       Some(err),
+        }
+    }
+}
+
+impl<I, V, E> ResultIterExt<V, E> for I where I: Iterator<Item = Result<V, E>> {}

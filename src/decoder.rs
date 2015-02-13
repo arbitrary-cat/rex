bed1a044ed5a1210571e8aadfa28c85cabba7df5
Arbitrary Cat <scpayson+devel@gmail.com>
2015-02-12 22:32:53 -0800

    Allow dead_code until the decoder is stable

--- src/decoder.rs
+++ src/decoder.rs
@@ -15,6 +15,9 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// Allow dead code to silence warnings until things stabilize.
+#![allow(dead_code)]
+
 use std::error::FromError;
 use std::io;
 use std::mem;

19e6661e852927e6efe8ebd2b47205c949738f03
Arbitrary Cat <scpayson+devel@gmail.com>
2015-02-13 13:37:26 -0800

    Fleshed out decoder.rs
    It now describes an ostensibly functional decoder.

--- src/decoder.rs
+++ src/decoder.rs
@@ -20,24 +20,249 @@
 
 use std::error::FromError;
 use std::io;
+use std::io::ReadExt;
 use std::mem;
 use std::result;
-use std::io::ReadExt;
+use std::string::FromUtf8Error;
+
+use encoding::{CompleteEncoding, RecordEncoding, FieldEncoding, Type, FieldID};
+use primitive::Primitive;
 
 use iter::{RexIterExt, ResultIterExt};
 
-/// `Error` is used to report errors that occur within the decoder.
+/// `Error` is used to report errors that occur during the decoding process.
 pub enum Error {
     /// `EOF` indicates that record source ended before it could be fully decoded.
     EOF,
 
-    /// `IoError` is used to pass through std::io errors.
+    /// `FieldTypeMismatch` indicates a disagreement between the `Decoder` and the `Decodable`
+    /// about what the type of a field is. This probably indicates one of two things:
+    ///
+    /// 1. The wrong `CompleteEncoding` is being used to decode this type.
+    /// 2. The `Decodable` implementation is not consistent with the record definition.
+    FieldTypeMismatch,
+
+    /// `EncodingInvalid` indicates that there is an inconsistency in the `CompleteEncoding` being
+    /// used. A probable fix is to regenerate the encoding (or inspect the encoding compiler for
+    /// errors).
+    EncodingInvalid,
+
+    /// `BadBool` indicates that a bool was read off the wire as a value other than `0xFF` or
+    /// `0x00`, indicating that the decoder is incorrect in expecting it to be a bool.
+    BadBool,
+
+    /// `Utf8Error` is used to pass through `std::str::FromUtf8Error`s.
+    Utf8Error(FromUtf8Error),
+
+    /// `IoError` is used to pass through `std::io` errors.
     IoError(io::Error),
 }
 
 impl FromError<io::Error> for Error {
     fn from_error(err: io::Error) -> Error {
         Error::IoError(err)
+    }
+}
+
+impl FromError<FromUtf8Error> for Error {
+    fn from_error(err: FromUtf8Error) -> Error {
+        Error::Utf8Error(err)
+    }
+}
+
+/// The `Decodable` trait allows an object to be decoded from a rex record.
+pub trait Decodable {
+    /// `set_primitive` sets the value of a single element of a field with primitive type.
+    fn set_primitive(&mut self, id: FieldID, idx: usize, prim: Primitive) -> Result<(), Error>;
+
+    /// `decode_record` is a request to call `d.decode` on a record field.
+    fn decode_record<'x, R>(&mut self, d: Decoder<'x, R>, id: FieldID, idx: usize) -> Result<(), Error>
+        where R: io::Read + 'x;
+
+    /// `alloc_field` requests that the receiver allocate space for an optional or repeated field.
+    /// The boolean return value can be `false` to indicate that the receiver is uninterested in
+    /// this field.
+    fn alloc_field(&mut self, id: FieldID, count: usize) -> Result<bool, Error>;
+}
+
+/// A `Decoder` is a struct that knows how to decode a particular record field. `Decoder`s should
+/// only be used where they are passed to the `decode_record` method of a `Decodable`.
+pub struct Decoder<'x, R: io::Read + 'x> {
+    // `r` is the reader that the record is being read from.
+    r: &'x mut R,
+
+    // `rec` is the record type that this `Decoder` knows how to decode.
+    rec: &'x RecordEncoding,
+
+    // `deps` is a (full) slice of the `depends` field of the `CompleteEncoding` that `rec` is a
+    // member of.
+    deps: &'x [RecordEncoding],
+}
+
+impl<'x, R> Decoder<'x, R>
+    where R: io::Read + 'x {
+
+    /// `decode` decodes the next record on the wire into `d`.
+    pub fn decode<D>(&mut self, d: &mut D) -> Result<(), Error>
+        where D: Decodable {
+
+        use encoding::Quantifier::*;
+
+        for req_field in self.rec.req_fields.iter() {
+            try!(self.decode_required(d, req_field));
+        }
+
+        let mut opt_rep_itr = self.rec.opt_rep_fields.iter();
+        let mut next_field  = opt_rep_itr.next();
+        let mut next_id     = FieldID(try!(read_uvarint(self.r)));
+
+        while next_id != FieldID(0) {
+            match next_field {
+                Some(field) => if field.id < next_id {
+                    next_field = opt_rep_itr.next();
+                } else if field.id > next_id {
+                    try!(self.skip_field());
+                    next_id = FieldID(try!(read_uvarint(self.r)));
+                } else {
+                    match field.quant {
+                        Required => return Err(Error::EncodingInvalid),
+                        Optional => try!(self.decode_optional(d, field)),
+                        Repeated => try!(self.decode_repeated(d, field)),
+                    }
+                    next_field = opt_rep_itr.next();
+                    next_id    = FieldID(try!(read_uvarint(self.r)));
+                },
+                None => next_id = FieldID(try!(read_uvarint(self.r))),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn skip_field(&mut self) -> Result<(), Error> {
+        let len = try!(read_uvarint(self.r)) as usize;
+        result::fold(self.r.bytes().take_or_err(len, Error::EOF), (), |(), _| ())
+    }
+
+    fn child(&mut self, index: usize) -> Result<Decoder<R>, Error> {
+        if index < self.deps.len() {
+            Ok( Decoder {
+                r:      self.r,
+                rec:    &self.deps[index],
+                deps:   self.deps,
+            })
+        } else {
+            Err(Error::EncodingInvalid)
+        }
+    }
+
+    fn decode_required<D>(&mut self, d: &mut D, f: &FieldEncoding) -> Result<(), Error>
+        where D: Decodable {
+
+        self.decode_array(d, f, 0)
+    }
+
+    fn decode_optional<D>(&mut self, d: &mut D, f: &FieldEncoding) -> Result<(), Error>
+        where D: Decodable {
+
+        // Discard the byte-size prefix. We don't need it.
+        try!(read_uvarint(self.r));
+
+        self.decode_array(d, f, 0)
+    }
+
+    fn decode_repeated<D>(&mut self, d: &mut D, f: &FieldEncoding) -> Result<(), Error>
+        where D: Decodable {
+
+        // Discard the byte-size prefix. We don't need it.
+        try!(read_uvarint(self.r));
+
+        let len = try!(read_uvarint(self.r)) as usize;
+
+        for idx in 0..len {
+            try!(self.decode_array(d, f, idx));
+        }
+
+        Ok(())
+    }
+
+
+
+    fn decode_array<D>(&mut self, d: &mut D, f: &FieldEncoding, idx: usize) -> Result<(), Error>
+        where D: Decodable {
+
+        if let Some(max) = f.bounds {
+            for arr_index in 0..max {
+                try!(self.decode_field(d, f, idx*max + arr_index));
+            }
+        } else {
+            try!(self.decode_field(d, f, idx));
+        }
+
+        Ok(())
+    }
+
+    fn decode_field<D>(&mut self, d: &mut D, f: &FieldEncoding, idx: usize) -> Result<(), Error>
+        where D: Decodable {
+
+        let prim = match f.typ {
+            Type::UInt8  => Primitive::UInt8(try!(read_u8(self.r))),
+            Type::UInt16 => Primitive::UInt16(try!(read_le_u16(self.r))),
+            Type::UInt32 => Primitive::UInt32(try!(read_uvarint(self.r)) as u32),
+            Type::UInt64 => Primitive::UInt64(try!(read_uvarint(self.r))),
+
+            Type::Int8  => Primitive::Int8(try!(read_i8(self.r))),
+            Type::Int16 => Primitive::Int16(try!(read_le_i16(self.r))),
+            Type::Int32 => Primitive::Int32(try!(read_varint(self.r)) as i32),
+            Type::Int64 => Primitive::Int64(try!(read_varint(self.r))),
+
+            Type::Fixed32 => Primitive::Fixed32(try!(read_le_u32(self.r))),
+            Type::Fixed64 => Primitive::Fixed64(try!(read_le_u64(self.r))),
+
+            Type::Float32 => Primitive::Float32(try!(read_le_f32(self.r))),
+            Type::Float64 => Primitive::Float64(try!(read_le_f64(self.r))),
+
+            Type::Bool => Primitive::Bool(match try!(read_u8(self.r)) {
+                0xFF => true,
+                0x00 => false,
+                _    => return Err(Error::BadBool),
+            }),
+
+            Type::Bytes => Primitive::Bytes({
+                let len = try!(read_uvarint(self.r)) as usize;
+                try!(result::fold(
+                    self.r.bytes().take_or_err(len, Error::EOF),
+                    Vec::with_capacity(len),
+                    |mut v, elem| {
+                        v.push(elem);
+                        v
+                }))
+            }),
+
+            Type::String => Primitive::String({
+                let len = try!(read_uvarint(self.r)) as usize;
+                let utf8 = try!(result::fold(
+                    self.r.bytes().take_or_err(len, Error::EOF),
+                    Vec::with_capacity(len),
+                    |mut v, elem| {
+                        v.push(elem);
+                        v
+                }));
+
+                try!(String::from_utf8(utf8))
+            }),
+
+            Type::Enum => Primitive::Enum(try!(read_varint(self.r))),
+
+            // Records work a little differently. Create a child decoder and have the `Decodable`
+            // run it on its own record field.
+            Type::Record{index: dep_index} => {
+                let child = try!(self.child(dep_index));
+                return d.decode_record(child, f.id, idx);
+            },
+        };
+
+        d.set_primitive(f.id, idx, prim)
     }
 }
 
@@ -48,7 +273,6 @@ fn read_uvarint<R>(r: &mut R) -> Result<u64, Error>
     let itr = r.bytes()
         .take_while_incl(|x| match x { &Ok(x) => x >= 0x80, _ => true });
 
-    // The Ok(try!(..)) just lets us convert from io::Error to Error
     result::fold(itr, 0, |sum, x| (sum << 7) + (x & 0x7F) as u64)
         .map_err(FromError::from_error)
 }
